9a718d8382a4d017409ffb792b4c85dd7d951ba1
Mubasher <m.mubasher1055@gmail.com>
2023-09-14 00:50:30 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
+import Nav from "./Components/Nav";
+import Textform from "./Components/Textform";
 
-import Nav from './Components/Nav'
-import Textform from './Components/Textform'
-
-import './App.css'
-import { useState } from 'react'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
 
-  const [mode , setMode] = useState('light')
-
-  const toggleMode = () =>{
-
-    if (mode === 'light'){
-      setMode('dark')
-    }
-    else
-    setMode('light')
-  }
- 
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+    } else setMode("light");
+    document.body.style.backgroundColor = "white";
+  };
 
   return (
     <>
-    <Nav title="Text-Utils" mode={mode} toggleMode = {toggleMode}  />
-    <Textform  heading = "Please Enter Text To Analyize" />
-     
+      <Nav title="Text-Utils" mode={mode} toggleMode={toggleMode} />
+      <Textform heading="Please Enter Text To Analyize" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
